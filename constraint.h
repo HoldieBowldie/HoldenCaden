beab692efd559f9d97b88d2b51df3563735b8c80
Caden Toczek <ctoczek@purdue.edu>
2024-03-25 20:43:27 -0400

    fix particle constructor, added distance calc and connect functions, added velocity to particles

--- constraint.h
+++ constraint.h
@@ -12,6 +12,7 @@ public:
 	// constructors
 	Constraint(void) {}
 
+	// w/ length parameter, just in case we ever want the default length to be different?
 	Constraint(Particle* _a, Particle* _b, GLfloat _k, GLfloat _length) {
 		a = _a;
 		b = _b;
@@ -19,8 +20,16 @@ public:
 		length = _length;
 	}
 
+	// calculates length from position of two particles
+	Constraint(Particle* _a, Particle* _b, GLfloat _k) {
+		a = _a;
+		b = _b;
+		k = _k;
+		length = a->distanceFrom(b);
+	}
+
 	// destructor
-	~Constraint(void) {} // empty for now !!
+	~Constraint(void) {}
 
 	// variables
 	Particle* a; // first endpoint, pointer to a particle
@@ -29,3 +38,10 @@ public:
 	GLfloat length; // default length of the constraint 
 
 };
+
+// form a constraint between two particles and add it to both of their constraint lists
+void connectParticles(Particle* a, Particle* b, GLfloat k) {
+	Constraint* c = new Constraint(a, b, k);
+	a->constraints->push_back(c);
+	b->constraints->push_back(c);
+}
